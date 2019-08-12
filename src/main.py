7d076669fc7487332ee50bfc08c419656d3ba7ad
niangxiu <niangxiu@gmail.com>
2019-08-12 10:16:14 -0700

    merge plot and main files by __name__

---
+++ src/main.py
@@ -0,0 +1,116 @@
+import network
+import mnist_loader
+import os.path
+import data_generator
+import os
+import numpy as np
+import pickle
+from pdb import set_trace
+import time
+
+
+def compare_stoptime():
+    # DIST all run
+    net_sizes = [50, 50, 50, 50]
+    data_sizes = [50000, 1, 10000]
+    training_data, validation_data, test_data = data_generator.load_data(net_sizes,data_sizes)
+
+    # # MNIST all run
+    # # net_sizes = [784, 30, 10]
+    # # training_data, test_data = mnist_loader.load_data_wrapper(validation = False)
+
+    # etas = [None, None, None, None,     None, None, None,   0.01, 0.03, 0.1, 0.3,     1, 3, 10, 30,                 100, 300, 1000]
+    # epsstars = [0.1, 0.15, 0.2, 0.3,    0.45, 0.67, 1.0,    None, None, None, None,   None, None,  None, None,      None, None,  None]
+    # mbs = [10, 10, 10, 10,              10, 10, 10,         10, 10, 10,10,            10, 10, 10, 10,               10, 10, 10]
+    # etas = [None, None, None, None,     None, None, None]
+    # epsstars = [0.1, 0.15, 0.2, 0.3,    0.45, 0.67, 1.0]
+    # mbs = [10, 10, 10, 10,              10, 10, 10]
+    etas =      [None, None, None,  None, None, None, None]
+    epsstars =  [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]
+    mbs =       [1, 2, 5, 10, 20, 50, 100]
+    assert len(etas) == len(mbs) == len(epsstars)
+    nruns = 1
+    nepoch = 50
+    all_stoptime = []
+
+    for eta, epsstar, mb in zip(etas, epsstars, mbs):
+        print('eta=', eta, ' eps*=', epsstar, 'minibatch_size=', mb)
+        results = []
+        for i in range(nruns):
+            time.sleep(1)
+            try:
+                os.remove('checkpoint.p')
+            except:
+                print("Error while deleting checkpoint")
+            try:
+                os.remove('record.txt')
+            except:
+                print("Error while deleting record file")
+            net = network.Network(net_sizes, generator=False, epsstar=epsstar)
+            j = net.SGD(training_data, epochs=nepoch, mini_batch_size=mb, test_data=test_data, case='DIST', const_eta=eta, target=0.1)
+            # _ = net.SGD(training_data, epochs=nepoch, mini_batch_size=mb, test_data=test_data, case='MNIST', const_eta=eta)
+            results.append(j)
+        results = np.array(results)
+        all_stoptime.append(results.mean(axis=0))
+
+    all_stoptime = np.array(all_stoptime)
+    with open("stoptime_batchsize.p", "wb") as f:
+        pickle.dump((epsstars, etas, mbs, all_stoptime), f)
+
+
+
+def compare_hist():
+    # DIST all run
+    # net_sizes = [10, 10, 10, 10, 10, 10, 10, 10]
+    # net_sizes = [50, 50, 50, 50]
+    # data_sizes = [50000, 1, 10000]
+    # training_data, validation_data, test_data = data_generator.load_data(net_sizes,data_sizes)
+
+    # # MNIST all run
+    net_sizes = [784, 30, 10]
+    training_data, test_data = mnist_loader.load_data_wrapper(validation = False)
+
+    # etas = [None, None, None, None, 0.1, 1, 10, 100]
+    # epsstars = [0.1, 0.3, 0.5, 0.8, None, None, None, None]
+    # mbs = [10, 10, 10, 10, 10, 10, 10, 10]
+    # etas = [None, None, None, None, None, None, None, None]
+    # epsstars = [0.1, 0.3, 0.5, 0.8, 0.1, 0.3, 0.5, 0.8]
+    # mbs = [2, 2, 2, 2, 50, 50, 50, 50]
+    # etas = [None, None, None, None, None]
+    # epsstars = [0.5, 0.4, 0.3, 0.2, 0.1]
+    # mbs = [10, 10, 10, 10, 10]
+    etas = [None,]
+    epsstars = [0.1,]
+    mbs = [10,] 
+    nruns = 1
+    nepoch = 20
+    all_hist = []
+
+    for eta, epsstar, mb in zip(etas, epsstars, mbs):
+        print('eta=', eta, ' eps*=', epsstar, 'minibatch_size=', mb)
+        results = []
+        for i in range(nruns):
+            time.sleep(1)
+            try:
+                os.remove('checkpoint.p')
+            except:
+                print("Error while deleting checkpoint")
+            try:
+                os.remove('record.txt')
+            except:
+                print("Error while deleting record file")
+            net = network.Network(net_sizes, generator=False, epsstar=epsstar)
+            # _ = net.SGD(training_data, epochs=nepoch, mini_batch_size=mb, test_data=test_data, case='DIST', const_eta=eta)
+            _ = net.SGD(training_data, epochs=nepoch, mini_batch_size=mb, test_data=test_data, case='MNIST', const_eta=eta)
+            results.append(_)
+        results = np.array(results)
+        all_hist.append(results.mean(axis=0))
+
+    all_hist = np.array(all_hist)
+    with open("all_history.p", "wb") as f:
+        pickle.dump(all_hist, f)
+        
+
+if __name__ == '__main__':
+    # compare_hist()
+    compare_stoptime()
--- src/plot.py
+++ src/plot.py
@@ -1,9 +1,13 @@
+# plot the stopping time for different eta and eps* for DIST
+
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pickle
 from pdb import set_trace
+from matplotlib.lines import Line2D
+
 
 plt.rc('axes', labelsize='xx-large',  labelpad=12)
 plt.rc('xtick', labelsize='xx-large')
@@ -12,71 +16,139 @@ plt.rc('legend', fontsize='xx-large')
 plt.rc('font', family='sans-serif')
 
 
-# # plot history of eta and linearity error of one sample
-# f = open('record_psi_initial_001.txt',"r")
-# lines = f.readlines()
-# eta_hist = []
-# linerr_hist = []
-# for x in lines:
-    # eta_hist.append(float(x.split(' ')[0]))
-    # linerr_hist.append(float(x.split(' ')[1]))
-# n = len(eta_hist)
-# f.close()
-
-# f = open('record_psi_initial_1.txt',"r")
-# lines = f.readlines()
-# eta_hist1 = []
-# linerr_hist1 = []
-# for x in lines:
-    # eta_hist1.append(float(x.split(' ')[0]))
-    # linerr_hist1.append(float(x.split(' ')[1]))
-# n1 = len(eta_hist1)
-# assert n1 == n
-# f.close()
-
-# plt.figure(figsize=[8,7])
-# plt.subplot(211)
-# plt.plot(np.arange(n)/50.0, eta_hist)
-# plt.plot(np.arange(n1)/50.0, eta_hist1)
-# plt.ylabel('$\psi$')
-# plt.gca().axes.get_xaxis().set_visible(False)
-# plt.subplot(212)
-# plt.semilogy(np.arange(n)/50.0, linerr_hist)
-# plt.semilogy(np.arange(n1)/50.0, linerr_hist1)
-# plt.ylabel('$\epsilon$')
-# plt.xlabel('epochs')
-# plt.tight_layout()
-# plt.legend(('$\psi_0=0.01$','$\psi_0=1$'), loc='lower right')
-# plt.savefig('step_nonlinmeas_hist.png')
-# plt.close()
+def plot_stoptime():
 
 
-# plot history of error for different eta and eps* for DIST/MNIST
-# styles = ['b-', 'r-', 'g-', 'k-','b:','r:','g:','k:']
-with open("all_history.p", "rb") as f:
-    all_hist = pickle.load(f)
+    with open("stoptime_stepsize.p", "rb") as f:
+        all_epsstars, all_etas, mbs, all_stoptime = pickle.load(f)
+    n = len(all_epsstars)
 
-plt.figure(figsize=(8,6))
-# for y in zip(all_hist, styles):
-    # plt.plot(y, s)
-for y in all_hist:
-    plt.plot(y)
-plt.grid
-plt.xlabel('epochs')
-plt.ylabel('objective')
-plt.legend(('$\epsilon^*=0.5$', '$\epsilon^*=0.4$', '$\epsilon^*=0.3$',
-            '$\epsilon^*=0.2$', '$\epsilon^*=0.1$', '$\psi=1$', '$\psi=10$', '$\psi=100$'),
-            loc='upper right')
-# plt.legend(('$N_s=2$, $\epsilon^*=0.1$', 
-            # '$N_s=2$, $\epsilon^*=0.3$', 
-            # '$N_s=2$, $\epsilon^*=0.5$', 
-            # '$N_s=2$, $\epsilon^*=0.8$', 
-            # '$N_s=50$, $\epsilon^*=0.1$', 
-            # '$N_s=50$, $\epsilon^*=0.3$', 
-            # '$N_s=50$, $\epsilon^*=0.5$', 
-            # '$N_s=50$, $\epsilon^*=0.8$'),
-            # loc='upper right')
-plt.tight_layout()
-plt.savefig('DIST_obj_hist.png')
-# plt.savefig('MNIST_obj_hist_different_mbsize.png')
-plt.close()
+    epsstars = [all_epsstars[i] for i in range(n) if all_epsstars[i] is not None]
+    stoptimes = [all_stoptime[i] for i in range(n) if all_epsstars[i] is not None]
+    fig = plt.figure(figsize=(5,4))
+    ax1 = fig.add_subplot(111)
+    ax1.plot(epsstars, stoptimes, 'r-', marker='.')
+    plt.xscale('log')
+    plt.xlabel('$\epsilon^*$')
+    plt.ylabel('stopping epoch')
+
+    etas = [all_etas[i] for i in range(n) if all_etas[i] is not None]
+    stoptimes = [all_stoptime[i] for i in range(n) if all_etas[i] is not None]
+    etas = [0.001,] + etas + [10000,]
+    stoptimes = [49,] + stoptimes + [49,]
+    ax2 = ax1.twiny()
+    ax2.plot(etas, stoptimes, 'k:.')
+    plt.xscale('log')
+    plt.xlabel('$\psi$')
+    plt.ylabel('stopping epoch')
+
+    custom_lines = [Line2D([0], [0], color='r', ls='-', marker='.'),
+                    Line2D([0], [0], color='k', ls=':', marker='.')]
+    plt.legend(custom_lines, ['linGrad', 'SGD'], loc='center right')
+    plt.tight_layout()
+    plt.savefig('DIST_stop_time_stepsize.png')
+    plt.close()
+
+
+
+    # with open("stoptime_batchsize.p", "rb") as f:
+        # all_epsstars, all_etas, all_mbs, all_stoptime = pickle.load(f)
+    # n = len(all_epsstars)
+
+    # epsstars = [all_epsstars[i] for i in range(n) if all_epsstars[i] is not None]
+    # stoptimes = [all_stoptime[i] for i in range(n) if all_epsstars[i] is not None]
+    # mbs = [all_mbs[i] for i in range(n) if all_epsstars[i] is not None]
+    # plt.figure(figsize=(5,4))
+    # plt.plot(mbs, stoptimes, 'r-', marker='.')
+
+    # etas = [all_etas[i] for i in range(n) if all_etas[i] is not None]
+    # stoptimes = [all_stoptime[i] for i in range(n) if all_etas[i] is not None]
+    # mbs = [all_mbs[i] for i in range(n) if all_etas[i] is not None]
+    # plt.plot(mbs, stoptimes, 'k:.')
+    # plt.xscale('log')
+
+    # plt.grid
+    # plt.xlabel('batch size')
+    # plt.ylabel('stopping epoch')
+    # plt.ylim(bottom=0)
+    # plt.legend(('linGrad', 'SGD'), loc='center right')
+    # plt.tight_layout()
+    # plt.savefig('DIST_stop_time_batchsize.png')
+    # plt.close()
+
+
+
+def plot_hist():
+
+    # # plot history of eta and linearity error of one sample
+    # f = open('record_psi_initial_001.txt',"r")
+    # lines = f.readlines()
+    # eta_hist = []
+    # linerr_hist = []
+    # for x in lines:
+        # eta_hist.append(float(x.split(' ')[0]))
+        # linerr_hist.append(float(x.split(' ')[1]))
+    # n = len(eta_hist)
+    # f.close()
+
+    # f = open('record_psi_initial_1.txt',"r")
+    # lines = f.readlines()
+    # eta_hist1 = []
+    # linerr_hist1 = []
+    # for x in lines:
+        # eta_hist1.append(float(x.split(' ')[0]))
+        # linerr_hist1.append(float(x.split(' ')[1]))
+    # n1 = len(eta_hist1)
+    # assert n1 == n
+    # f.close()
+
+    # plt.figure(figsize=[8,7])
+    # plt.subplot(211)
+    # plt.plot(np.arange(n)/50.0, eta_hist)
+    # plt.plot(np.arange(n1)/50.0, eta_hist1)
+    # plt.ylabel('$\psi$')
+    # plt.gca().axes.get_xaxis().set_visible(False)
+    # plt.subplot(212)
+    # plt.semilogy(np.arange(n)/50.0, linerr_hist)
+    # plt.semilogy(np.arange(n1)/50.0, linerr_hist1)
+    # plt.ylabel('$\epsilon$')
+    # plt.xlabel('epochs')
+    # plt.tight_layout()
+    # plt.legend(('$\psi_0=0.01$','$\psi_0=1$'), loc='lower right')
+    # plt.savefig('step_nonlinmeas_hist.png')
+    # plt.close()
+
+
+    # plot history of error for different eta and eps* for DIST/MNIST
+    # styles = ['b-', 'r-', 'g-', 'k-','b:','r:','g:','k:']
+    with open("all_history.p", "rb") as f:
+        all_hist = pickle.load(f)
+
+    plt.figure(figsize=(8,6))
+    # for y in zip(all_hist, styles):
+        # plt.plot(y, s)
+    for y in all_hist:
+        plt.plot(y)
+    plt.grid
+    plt.xlabel('epochs')
+    plt.ylabel('objective')
+    plt.legend(('$\epsilon^*=0.5$', '$\epsilon^*=0.4$', '$\epsilon^*=0.3$',
+                '$\epsilon^*=0.2$', '$\epsilon^*=0.1$', '$\psi=1$', '$\psi=10$', '$\psi=100$'),
+                loc='upper right')
+    # plt.legend(('$N_s=2$, $\epsilon^*=0.1$', 
+                # '$N_s=2$, $\epsilon^*=0.3$', 
+                # '$N_s=2$, $\epsilon^*=0.5$', 
+                # '$N_s=2$, $\epsilon^*=0.8$', 
+                # '$N_s=50$, $\epsilon^*=0.1$', 
+                # '$N_s=50$, $\epsilon^*=0.3$', 
+                # '$N_s=50$, $\epsilon^*=0.5$', 
+                # '$N_s=50$, $\epsilon^*=0.8$'),
+                # loc='upper right')
+    plt.tight_layout()
+    plt.savefig('DIST_obj_hist.png')
+    # plt.savefig('MNIST_obj_hist_different_mbsize.png')
+    plt.close()
+
+if __name__ == '__main__':
+    # plot_hist()
+    plot_stoptime()
